25a6924b11ce9a5b2e3fb769e5bcc6e09c5ebc4a
xinyong <xinyong@shandagames.com>
2021-08-18 12:29:01 +0800

    优化windows下函数名称的显示

--- include/log.h
+++ include/log.h
@@ -4,16 +4,16 @@
 
 #define ASYNCIO_LOG_DEBUG(fmt, ...)                                                                                 \
 	asyncio::g_log->DoLog(asyncio::Log::kDebug, "[%s:%d %s()] " fmt, asyncio::Log::GetFileName(__FILE__), __LINE__, \
-						  __FUNCTION__, ##__VA_ARGS__)
+						  __func__, ##__VA_ARGS__)
 #define ASYNCIO_LOG_INFO(fmt, ...)                                                                                 \
 	asyncio::g_log->DoLog(asyncio::Log::kInfo, "[%s:%d %s()] " fmt, asyncio::Log::GetFileName(__FILE__), __LINE__, \
-						  __FUNCTION__, ##__VA_ARGS__)
+						  __func__, ##__VA_ARGS__)
 #define ASYNCIO_LOG_WARN(fmt, ...)                                                                                    \
 	asyncio::g_log->DoLog(asyncio::Log::kWarning, "[%s:%d %s()] " fmt, asyncio::Log::GetFileName(__FILE__), __LINE__, \
-						  __FUNCTION__, ##__VA_ARGS__)
+						  __func__, ##__VA_ARGS__)
 #define ASYNCIO_LOG_ERROR(fmt, ...)                                                                                 \
 	asyncio::g_log->DoLog(asyncio::Log::kError, "[%s:%d %s()] " fmt, asyncio::Log::GetFileName(__FILE__), __LINE__, \
-						  __FUNCTION__, ##__VA_ARGS__)
+						  __func__, ##__VA_ARGS__)
 
 namespace asyncio {
 
