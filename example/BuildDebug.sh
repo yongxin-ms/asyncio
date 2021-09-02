1ebbee5a2fbfc2cf232bc32faa41ad5838251592
xinyong <xinyong@shandagames.com>
2021-09-02 11:24:51 +0800

    为降低使用难度，内置了asio

--- example/BuildDebug.sh
+++ example/BuildDebug.sh
@@ -3,7 +3,7 @@ basepath=$(cd `dirname $0`; pwd)
 dir=$basepath/build
 mkdir -p "$dir"
 cd "$dir"
-cmake "-DCMAKE_BUILD_TYPE=Debug" "-DCMAKE_TOOLCHAIN_FILE=~/.vcpkg/scripts/buildsystems/vcpkg.cmake" ..
-#cmake --build .
+#cmake "-DCMAKE_BUILD_TYPE=Debug" "-DCMAKE_TOOLCHAIN_FILE=~/.vcpkg/scripts/buildsystems/vcpkg.cmake" ..
+cmake "-DCMAKE_BUILD_TYPE=Debug" ..
 make -j8
 #scl enable devtoolset-7 "make -j 16"
--- example/BuildRelease.sh
+++ example/BuildRelease.sh
@@ -3,6 +3,7 @@ basepath=$(cd `dirname $0`; pwd)
 dir=$basepath/build_release
 mkdir -p "$dir"
 cd "$dir"
-cmake "-DCMAKE_BUILD_TYPE=Release" "-DCMAKE_TOOLCHAIN_FILE=~/.vcpkg/scripts/buildsystems/vcpkg.cmake" ..
+#cmake "-DCMAKE_BUILD_TYPE=Release" "-DCMAKE_TOOLCHAIN_FILE=~/.vcpkg/scripts/buildsystems/vcpkg.cmake" ..
+cmake "-DCMAKE_BUILD_TYPE=Release" ..
 make -j8
 #scl enable devtoolset-7 "make -j 16"
