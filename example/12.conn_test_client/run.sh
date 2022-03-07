3ae1ebbace9adf6fb4599bfabb97c786fe0dbf96
xinyong <xinyong@shandagames.com>
2022-03-07 15:51:20 +0800

    让windows和linux下的脚本配置保持一致

--- example/12.conn_test_client/run.sh
+++ example/12.conn_test_client/run.sh
@@ -2,4 +2,4 @@
 basepath=$(cd `dirname $0`; pwd)
 ulimit -c unlimited
 
-$basepath/../bin/ConnTestClient 127.0.0.1 9000 1
+$basepath/../bin/ConnTestClient 127.0.0.1 9000 100
--- example/5.load_test_client/run.sh
+++ example/5.load_test_client/run.sh
@@ -2,4 +2,4 @@
 basepath=$(cd `dirname $0`; pwd)
 ulimit -c unlimited
 
-$basepath/../bin/LoadTestClient 127.0.0.1 9000 1
+$basepath/../bin/LoadTestClient 127.0.0.1 9000 100
